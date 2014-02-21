6ae11478f1c2e76ace26a52784d8fa0d5aea792e
macember <macember@vassar.edu>
2014-02-21 01:16:52 -0500

    added pickle and depickle

--- NN.py
+++ NN.py
@@ -1,10 +1,16 @@
-import random
+import random, json
+
+def createTestNN():
+    N = NN()
+    N.readInputOld('neuralNetDNA.txt')
+    return N
+    
 
 class Node:
     def __init__(self, Name, Layer=0, Delta=0, Connections={}):
         self.name = Name
-        self.layer = Layer
-        self.delta = Delta
+        self.layer = int(Layer)
+        self.delta = float(Delta)
         self.connections = Connections
         self.output = 0
 
@@ -16,12 +22,35 @@ class Node:
         print("and our output is:", self.output)
 
 
-
 class NN:
     def __init__(self, dna=""):
         self.nodes = {}
         self.DNA = dna
         random.seed()
+
+    def pickle(self): #returns a dictionary of the NN. Stores info about NN
+        ret = {}
+        nodesDict = {}
+        for node in self.nodes.values(): #iterate through each node
+            key = node.name
+            val = node.__dict__
+            nodesDict.update({key:val})
+        ret.update({"nodes":nodesDict})
+        return str(ret)
+
+    def depickle(self, datafile):
+        DNAstring = readFromFile(datafile)
+        NNdict = eval(DNAstring)
+        self.nodes = {}
+        for n in NNdict['nodes'].values():
+            name = n['name']
+            layer = n['layer']
+            delta = n['delta']
+            connections = n['connections']
+            node = Node(name,layer,delta,connections)
+            self.nodes.update({name:node})
+            
+                
 
     def randNN(self, layers):
         #input: layers is an array defining the size of each layer
@@ -30,13 +59,14 @@ class NN:
         if(totalNodes>26 or totalNodes<1):
             print('You aint got enough nodes, muthafucka. You got ', totalNodes, " nodes")
             return        
-        nodeNames = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        nodeNames = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
         ###the number of random connections we will need to generate is layers[0]*layers[1] + layers[1]*layers[2]...layers[n-1]layers[n]
         allConnections = [] 
-        for i in range(1,len(layers))
+        for i in range(1,len(layers)):
             for j in range(0, layers[i]*layers[i+1]):
-            #    allConnections.append(    
+                print('test')
+            #    allConnections.append(
 
         nodeIndex = 1
         layerNumber = 0
@@ -45,47 +75,27 @@ class NN:
             for i in range(1, layer+1):
                      nodexIndex+=1
                      n = node(nodeNames[nodeIndex], layerNumber)
-                     n.connections = 
+                     #n.connections = 
                      
-                     
-        
-
-    def readInput(self, datafile):
-        f = open(datafile, 'r')
-        DNAstring = f.read()
-        f.close()
-        #first, parse each node
-        allNodes = DNAstring.split(";")
-        for nodey in allNodes:
-            #print("nodey is: ",nodey)
-            #creating an individual node
-            n = nodey.partition(":")
-           # print("n is: ", n)
-            name = n[0]
-            data = n[2].split(",") #each data element is separated by a comma
-            layer = data[0]
-            delta = data[1]
-            connections = {}
-            connectionList = data[2].split("#")
-            print("connectionList is: ", connectionList)
-            for c in connectionList:
-                x = c.partition("-")
-                name = x[0]
-                weight = x[2]
-                connections.update({name:weight})
-                print("updating connection with ", name, weight)
-            #now we have all the data for a node, create a node object
-            print("Done with node ", name, " Connections are ", connections)
-            n = Node(name, layer, delta, connections)
-            self.nodes.update({name:n})
             
     def printNN(self):
         for nodey in self.nodes.values():
             print("Printing node ", nodey.name)
             nodey.printNode()
                 
+    #def feedForward(self):
+        
+def writeToFile(filename, toWrite):
+    f = open(filename, 'w')
+    f.write(toWrite)
+    f.close()
 
-
+def readFromFile(filename):
+    f = open(filename, 'r')
+    string = f.read()
+    f.close()
+    return string
+    
 #TODO- 1. write parser for NN DNA
     #  2. write way to generate random NN 
     #  3. write way to mutate NN 
